339021c51a1b010119ceb2e628b8f041763e46e7
marcus lucious <marcus.j.lucious@outlook.com>
2018-09-06 00:37:22 -0400

    Add project files.

---
+++ MyFarm/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFarm
+{
+    class Animal
+    {
+        
+
+        public string name;
+
+        public Animal()
+        {
+           // string pig = "oink";
+            //string chicken;
+            //string cow;
+            string goat;
+
+        }
+    }
+}
---
+++ MyFarm/Program.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFarm
+{
+  
+
+    class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            string c = "Chicken";
+            string a = "Cow";
+            string g = "Goat";
+            string p = "Pig";
+
+
+            Animal animal = new Animal();
+
+
+            Console.WriteLine("Welcome to myFarm");
+            Console.WriteLine("On myfarm we have " +c+ " ," +a+ " ," +g+ " and " + p);
+
+            Console.WriteLine("We have these animals on myFarm");
+            //Animal.name = "Chicken, Cow, Goat, Pig";
+            Chicken(animal);
+            Cow(animal);
+            Goat(animal);
+            Pig(animal);
+
+
+
+          
+
+        }
+
+        private static void Pig(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Goat(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Cow(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Chicken(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
